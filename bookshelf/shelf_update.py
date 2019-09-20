c25573e9aaa51d0317206ec64c9fcfcb7c3646a5
wilenwu <597379201@qq.com>
2019-09-20 18:10:38 +0800

    Site updated: 2019-09-20 18:10:24

---
+++ bookshelf/shelf_update.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+
+import os
+import re 
+
+with open('../../_config.yml','r') as f:
+    config=f.readlines()
+	
+for line in config:
+    
+  
+
+os.list.dir()
+
+permalink
+
+read _config,yml
+site_url
+
+post_name
+title
+link
+\t
+
+write post_info
